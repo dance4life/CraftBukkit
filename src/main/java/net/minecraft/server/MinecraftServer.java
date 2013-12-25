068d81eae89e7cf6bbdedb79938016742adfa9e6
dance4life <sarahsassin@icloud.com>
2013-12-24 21:42:56 -0500

    Update MinecraftServer.java
    welcome to GolemRims! JOIN NOW:)

--- src/main/java/net/minecraft/server/MinecraftServer.java
+++ src/main/java/net/minecraft/server/MinecraftServer.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+import java.util.Random; 
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import javax.imageio.ImageIO;
